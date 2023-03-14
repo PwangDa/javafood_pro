2b35a2f7a4cba576ab69b690f91b21b6b03ae17a
김범주 <rlaqjawn416@gmail.com>
2023-03-14 14:47:01 +0900

    JavafoodController 수정 중

--- src/main/java/com/java/food/controller/JavafoodController.java
+++ src/main/java/com/java/food/controller/JavafoodController.java
@@ -1,7 +1,10 @@
 package com.java.food.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,13 +24,25 @@ public class JavafoodController {
 		//귀범
 	}
 ////////////////////////////////////////////////////////////
+	//범주
 	@RequestMapping("playList")
-	public void selectPlayList(HttpServletRequest request)
+	public String selectPlayList(HttpServletRequest request)
 	{
-		//범주
-		System.out.println("JavafoodController의 selectPlayList 메서드 실행됨.");//확인용
-		System.out.println("해당 플레이 리스트를 요청한 아이디 : " + request.getSession().getAttribute("login") );
+		String result = "playList/playList"; 
 		
+		System.out.println("JavafoodController의 selectPlayList 메서드 실행됨."); //확인용
+		
+		//세션에 저장된 id값 받아오기
+		String id = (String)request.getSession().getAttribute("login");
+		System.out.println("해당 플레이 리스트를 요청한 아이디 : " + id ); //확인용
+		
+		//Service에서 플레이 리스트를 불러오는 메서드 실행하기
+		//메서드 실행 결과(리스트)를 리스트에 담기
+		
+		List playList = 
+				
+	
+		return result;
 	}
 ////////////////////////////////////////////////////////////
 	//경용
