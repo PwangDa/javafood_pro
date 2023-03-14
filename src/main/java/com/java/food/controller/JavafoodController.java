457ca1c5eb9126e2a03975c1468f2bfb31e2027a
이경용 <93perfect@naver.com>
2023-03-14 12:31:13 +0900

    주석

--- src/main/java/com/java/food/controller/JavafoodController.java
+++ src/main/java/com/java/food/controller/JavafoodController.java
@@ -2,27 +2,34 @@ package com.java.food.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class JavafoodController {
+////////////////////////////////////////////////////////////
 	@RequestMapping
 	public void java1() {
 		//다영
 	}
+////////////////////////////////////////////////////////////
 	@RequestMapping
 	public void java2() {
 		//귀범
 	}
+////////////////////////////////////////////////////////////
 	@RequestMapping
 	public void java3() {
 		//범주
 	}
-	@RequestMapping
+////////////////////////////////////////////////////////////
+	@RequestMapping(value = "login/mypage", method = RequestMethod.GET)
 	public void java4() {
 		//경용
 	}
+////////////////////////////////////////////////////////////
 	@RequestMapping
 	public void java5() {
 		//용준
 	}
+////////////////////////////////////////////////////////////
 }
