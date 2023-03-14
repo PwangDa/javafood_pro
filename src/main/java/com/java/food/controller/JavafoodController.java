d1b7f2622f85641d440e724541455df78581cbab
이경용 <93perfect@naver.com>
2023-03-14 12:23:21 +0900

    세팅

--- src/main/java/com/java/food/controller/JavafoodController.java
+++ src/main/java/com/java/food/controller/JavafoodController.java
@@ -6,7 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JavafoodController {
 	@RequestMapping
-	public void login() {
-		
+	public void java1() {
+		//다영
+	}
+	@RequestMapping
+	public void java2() {
+		//귀범
+	}
+	@RequestMapping
+	public void java3() {
+		//범주
+	}
+	@RequestMapping
+	public void java4() {
+		//경용
+	}
+	@RequestMapping
+	public void java5() {
+		//용준
 	}
 }
