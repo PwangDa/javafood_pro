58bdd429c7b726622f8a53a2bf601bfff3a0f414
최귀범 <cgb416@naver.com>
2023-03-14 12:50:22 +0900

    logger 추가 및 Session 추가

--- src/main/java/com/java/food/dao/JavafoodDAOImpl.java
+++ src/main/java/com/java/food/dao/JavafoodDAOImpl.java
@@ -1,7 +1,19 @@
 package com.java.food.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class JavafoodDAOImpl implements JavafoodDAO {
 	
+private static final Logger logger = LoggerFactory.getLogger(JavafoodDAOImpl.class);
+
+@Autowired
+SqlSession sqlSession;
+
 ////////////////////////////////////////////////////////////
 //다영
 
