ef6950108c9182776f046ae5bf880e7f535cb576
최귀범 <cgb416@naver.com>
2023-03-14 11:44:11 +0900

    인기차트 DTO 파일 추가

---
+++ src/main/java/com/java/food/dto/FamousChartDTO.java
@@ -0,0 +1,130 @@
+package com.java.food.dto;
+
+public class FamousChartDTO {
+	
+	private String songnumber;
+	private String artistname;
+	private String songname;
+	private String bygenre;
+	private String hits;
+	private String likes;
+	private String link;
+	private String famous;
+	private String record;
+	private String playtime;
+	private String ranking;
+	private String album;
+	private String imglink;
+	private String country;
+	private String album_name;
+	private String album_add;
+	private String artist_add;
+	
+	
+	public String getSongnumber() {
+		return songnumber;
+	}
+	public void setSongnumber(String songnumber) {
+		this.songnumber = songnumber;
+	}
+	public String getArtistname() {
+		return artistname;
+	}
+	public void setArtistname(String artistname) {
+		this.artistname = artistname;
+	}
+	public String getSongname() {
+		return songname;
+	}
+	public void setSongname(String songname) {
+		this.songname = songname;
+	}
+	public String getBygenre() {
+		return bygenre;
+	}
+	public void setBygenre(String bygenre) {
+		this.bygenre = bygenre;
+	}
+	public String getHits() {
+		return hits;
+	}
+	public void setHits(String hits) {
+		this.hits = hits;
+	}
+	public String getLikes() {
+		return likes;
+	}
+	public void setLikes(String likes) {
+		this.likes = likes;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getFamous() {
+		return famous;
+	}
+	public void setFamous(String famous) {
+		this.famous = famous;
+	}
+	public String getRecord() {
+		return record;
+	}
+	public void setRecord(String record) {
+		this.record = record;
+	}
+	public String getPlaytime() {
+		return playtime;
+	}
+	public void setPlaytime(String playtime) {
+		this.playtime = playtime;
+	}
+	public String getRanking() {
+		return ranking;
+	}
+	public void setRanking(String ranking) {
+		this.ranking = ranking;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getImglink() {
+		return imglink;
+	}
+	public void setImglink(String imglink) {
+		this.imglink = imglink;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getAlbum_name() {
+		return album_name;
+	}
+	public void setAlbum_name(String album_name) {
+		this.album_name = album_name;
+	}
+	public String getAlbum_add() {
+		return album_add;
+	}
+	public void setAlbum_add(String album_add) {
+		this.album_add = album_add;
+	}
+	public String getArtist_add() {
+		return artist_add;
+	}
+	public void setArtist_add(String artist_add) {
+		this.artist_add = artist_add;
+	}
+	
+	
+	
+	
+}
